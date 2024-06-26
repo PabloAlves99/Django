ab8d3c60468eb76f7adbdcb228e6d3baf07e9aeb
Pablo <PabloHAlves99@gmail.com>
2024-06-25 22:12:26 -0300

    New up

--- recipes/views.py
+++ recipes/views.py
@@ -6,6 +6,7 @@ from .models import Recipe
 
 def home(request):
     recipes = Recipe.objects.filter(is_published='True').order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -16,8 +17,8 @@ def category(request, category_id):
         Recipe.objects.filter(
             category__id=category_id,
             is_published=True,
-        ).order_by('-id')
-    )
+        ).order_by('-id'))
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'title': f'{recipes[0].category.name} - Category | '
@@ -25,7 +26,12 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
