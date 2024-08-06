d60252317696c7e90e3d61c018a2d415ce9dfd7d
Pablo <PabloHAlves99@gmail.com>
2024-08-06 19:14:46 -0300

    Update browser.py

--- utils/authors/browser.py
+++ utils/authors/browser.py
@@ -9,9 +9,21 @@ ROOT_PATH = Path(__file__).parent.parent.parent
 CHROMEDRIVER_NAME = 'chromedriver.exe'
 CHROMEDRIVER_PATH = ROOT_PATH / 'bin' / CHROMEDRIVER_NAME
 
-chrome_options = webdriver.ChromeOptions()
-chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
-browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-browser.get('http://127.0.0.1:8000/')
-sleep(10)
+def make_chrome_browser(*options):
+    chrome_options = webdriver.ChromeOptions()
+
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
+    browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    return browser
+
+
+if __name__ == '__main__':
+    browser = make_chrome_browser()
+    browser.get('http://127.0.0.1:8000/')
+    sleep(5)
+    browser.quit()
