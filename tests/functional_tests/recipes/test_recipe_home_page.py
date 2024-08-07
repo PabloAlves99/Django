f2141065760a9bbb0399a9ab326998ff29c1f7b0
Pablo <PabloHAlves99@gmail.com>
2024-08-07 01:25:58 -0300

    Update test_recipe_home_page.py

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -10,8 +10,3 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('No recipes found here 🥲', body.text)
-
-
-class RecipeTestBase(TestCase, RecipeMixin):
-    def setUp(self) -> None:
-        return super().setUp()
