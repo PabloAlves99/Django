477f9178db0a9b3eed783893ea92168a48e84ef7
Pablo <PabloHAlves99@gmail.com>
2024-08-07 01:05:19 -0300

    Update browser.py

--- utils/authors/browser.py
+++ utils/authors/browser.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from time import sleep
+import os
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -17,9 +18,12 @@ def make_chrome_browser(*options):
         for option in options:
             chrome_options.add_argument(option)
 
+    if os.environ.get('SELENIUM_HEADLESS') == '1':
+        chrome_options.add_argument('--headless')
+
     chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
-    browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
-    return browser
+    _browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    return _browser
 
 
 if __name__ == '__main__':
