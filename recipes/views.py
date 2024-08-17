7421175a01914c14a29d40b8098c3aadca447852
Pablo <PabloHAlves99@gmail.com>
2024-08-16 22:53:00 -0300

    Update views.py

--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 from django.http.response import Http404
 from django.http import JsonResponse
 from django.views.generic import DetailView, ListView
+from django.forms.models import model_to_dict
 from utils.recipes.pagination import make_pagination
 from .models import Recipe
 
@@ -125,3 +126,26 @@ class RecipeDetail(DetailView):
         })
 
         return ctx
+
+
+class RecipeDetailAPI(RecipeDetail):
+    def render_to_response(self, context, **response_kwargs):
+        recipe = self.get_context_data()['recipe']
+        recipe_dict = model_to_dict(recipe)
+
+        recipe_dict['created_at'] = str(recipe.created_at)
+        recipe_dict['updated_at'] = str(recipe.updated_at)
+
+        if recipe_dict.get('cover'):
+            recipe_dict['cover'] = self.request.build_absolute_uri() + \
+                recipe_dict['cover'].url[1:]
+        else:
+            recipe_dict['cover'] = ''
+
+        del recipe_dict['is_published']
+        del recipe_dict['preparation_steps_is_html']
+
+        return JsonResponse(
+            recipe_dict,
+            safe=False,
+        )
